67ee43695c8c729ca22fe3bdd313abf1ccf0dbfd
swesda <swesda@yandex.ru>
2022-06-21 20:37:21 +0300

    Miles

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 70;
+        int x = 20;
+        int bonus = amount / x;
+
+        if (bonus >= 20);
+
+        System.out.println("Итоговый бонус: " + bonus);
+    }
+}
